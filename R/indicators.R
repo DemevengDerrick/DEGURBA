b1d3431bba93a1b50ea37dfecab310d06cbc11a4
DemevengDerrick <demeveng@gmail.com>
2025-04-21 20:22:51 +0300

    updated

--- R/indicators.R
+++ R/indicators.R
@@ -2,7 +2,7 @@
 # LOAD LIBRARIES ----------------------------------------------------------
 if(!require("pacman")) install.packages("pacman")
 
-pacman::p_load(tidyverse, haven)
+pacman::p_load(tidyverse, haven, forcats)
 
 # LOAD DATA ---------------------------------------------------------------
 
@@ -14,71 +14,108 @@ demographics <- haven::read_sav("input/DataExercise/SPSS files/DemographicsDIST.
 
 # INDICATORS CALCULATION --------------------------------------------------
 
-##  INDICATOR: Women 18-24 with Child Marriage
+##  I - EMPOWERMENT INDICATOR: Child Marriage
 
-# (a) Check missing value - Totally 96919 people, 46405 are missing data
-# Variable Age at the first marriage: there are 46405 (47.9%) missing data
-# Variable Marital Status: there are 39839 people “Never married” and 6566 missing data. These two group people did not answer “Age at the first marriage”. (39839+6566=46405)
-# In this research, the 46405 people are included in the denominator, i.e. the total 96919 women at age of 18-24 is the denominator.
+# Indicator Definition :
+# number of women aged 18-24 who were married before age 18 / total number of women aged 18-24 * 100 
 
-df_18_24 <- demographics |>
-  dplyr::filter(P5_AGE_P >= 18, P5_AGE_P <= 24, P4_SEX_P == 2)
+# Variables :
+# - P4_SEX_P = Sex (1 - Male, 2 - Female)
+# - P5_AGE_P = Age (Between 18 & 24)
+# - DIST_P = District
 
-summary(df_18_24$P37_AGE_FIRST_MARRAIGE_P)
-
-# (b) Calculate child marriage at district level, first calculate total girls 18-24 using "Filter", then calculate girls 18-24 married before 18 using "Filter".
-
-df_18_24 |>
+# (a) Desegregate By District
+demographics |>
+  dplyr::filter(P5_AGE_P >= 18, P5_AGE_P <= 24, P4_SEX_P == 2) |>
   dplyr::group_by(DIST_P) |>
   dplyr::summarise(
     total = n(),
-    married_18_above = sum(P37_AGE_FIRST_MARRAIGE_P > 17, na.rm = T),
-    married_before_18 = sum(P37_AGE_FIRST_MARRAIGE_P <= 17, na.rm = T),
+    married_18_above = sum(P37_AGE_FIRST_MARRAIGE_P >= 18, na.rm = T), # what is the rational for looking at marriage before 18 and marriage after 18?
+    married_before_18 = sum(P37_AGE_FIRST_MARRAIGE_P < 18, na.rm = T),
     married_NA = sum(is.na(P37_AGE_FIRST_MARRAIGE_P)),
     rate_18_above = round(married_18_above / total, 2),
     rate_before_18 = round(married_before_18 / total, 2),
     rate_NA = round(married_NA / total, 2)
   ) |>
+  dplyr::mutate(
+    DIST_P_name = forcats::as_factor(DIST_P)
+  ) |>
+  dplyr::select(
+    DIST_P_name,
+    everything()
+  ) |>
   view()
 
 
-# (c)) Disagregate by Rual/Urban
+# (b) Desegregate by District & Rural/Urban
 
 # long table
-df_18_24 |>
+demographics |>
+  dplyr::filter(P5_AGE_P >= 18, P5_AGE_P <= 24, P4_SEX_P == 2) |>
   dplyr::group_by(DIST_P, REGION_P) |>
   dplyr::summarise(
     total = n(),
-    married_18_above = sum(P37_AGE_FIRST_MARRAIGE_P > 17, na.rm = T),
-    married_before_18 = sum(P37_AGE_FIRST_MARRAIGE_P <= 17, na.rm = T),
+    married_18_above = sum(P37_AGE_FIRST_MARRAIGE_P >= 18, na.rm = T), # what is the rational for looking at marriage before 18 and marriage after 18?
+    married_before_18 = sum(P37_AGE_FIRST_MARRAIGE_P < 18, na.rm = T),
     married_NA = sum(is.na(P37_AGE_FIRST_MARRAIGE_P)),
     rate_18_above = round(married_18_above / total, 2),
     rate_before_18 = round(married_before_18 / total, 2),
     rate_NA = round(married_NA / total, 2)
+  ) |>
+  dplyr::mutate(
+    DIST_P_name = forcats::as_factor(DIST_P),
+    REGION_P_name = forcats::as_factor(REGION_P)
+  ) |>
+  dplyr::select(
+    DIST_P_name,
+    REGION_P_name,
+    everything()
   ) |>
   view()
 
 # wide table
-df_18_24 |>
+demographics |>
+  dplyr::filter(P5_AGE_P >= 18, P5_AGE_P <= 24, P4_SEX_P == 2) |>
   dplyr::group_by(DIST_P, REGION_P) |>
   dplyr::summarise(
     total = n(),
-    married_18_above = sum(P37_AGE_FIRST_MARRAIGE_P > 17, na.rm = T),
-    married_before_18 = sum(P37_AGE_FIRST_MARRAIGE_P <= 17, na.rm = T),
+    married_18_above = sum(P37_AGE_FIRST_MARRAIGE_P >= 18, na.rm = T), # what is the rational for looking at marriage before 18 and marriage after 18?
+    married_before_18 = sum(P37_AGE_FIRST_MARRAIGE_P < 18, na.rm = T),
     married_NA = sum(is.na(P37_AGE_FIRST_MARRAIGE_P)),
     rate_18_above = round(married_18_above / total, 2),
     rate_before_18 = round(married_before_18 / total, 2),
     rate_NA = round(married_NA / total, 2)
   ) |>
+  dplyr::mutate(
+    DIST_P_name = forcats::as_factor(DIST_P),
+    REGION_P = forcats::as_factor(REGION_P)
+  ) |>
+  dplyr::select(
+    DIST_P_name,
+    REGION_P,
+    everything()
+  ) |>
   tidyr::pivot_wider(
     names_from =  REGION_P,
-    values_from = 3:9
+    values_from = 4:10
   ) |>
   view()
 
 
-## INDICATOR: Currently Attending Secondary School
+## II - EDUCATION INDICATOR: Secondary school Net Attendance Ratio (NAR)
 
+# Indicator Definition :
+# number of youth aged 14-18, attending secondary school (which means, completed grade 7-11) / total number of youth aged 14-18 * 100 
+# According to DHR report: 
+# - secondary school age are 14-18 years
+# - secondary school grade are 8-12)
+
+# Variables :
+# - P28_HIGHEST_LEVEL_P = 
+# - P27_ATTENDING_SCHOOL_P = 
+# - P26_EVER_ATTEND_SCHOOL_P =
+
+# (a) Desegregate By District
 demographics |>
   dplyr::mutate(
     P28Sec = case_when(
@@ -92,17 +129,100 @@ demographics |>
     )
   ) |>
   dplyr::filter(P5_AGE_P >= 14, P5_AGE_P <= 18) |>
-  dplyr::group_by(DIST_P) %>%
+  dplyr::group_by(DIST_P) |>
   dplyr::summarise(
     total = n(),
     attending_sec = sum(SecSchAtt == 1, na.rm = TRUE),
-    rate = attending_sec / total
+    rate = round(attending_sec / total, 2)
+  ) |>
+  dplyr::mutate(
+    DIST_P_name = forcats::as_factor(DIST_P),
+  ) |>
+  dplyr::select(
+    DIST_P_name,
+    everything()
   ) |>
   view()
 
 
-## INDICATOR: NEET (Not in Education, Employment or Training) 19–24
+# (b) Desegregate by District & Rural/Urban
 
+# long table
+demographics |>
+  dplyr::mutate(
+    P28Sec = case_when(
+      P28_HIGHEST_LEVEL_P %in% 7:11 ~ 1,
+      TRUE ~ 2
+    ),
+    SecSchAtt = case_when(
+      P27_ATTENDING_SCHOOL_P == 1 & P28Sec == 2 ~ 3, # attending school but not in secondary
+      P26_EVER_ATTEND_SCHOOL_P == 2 ~ 4, # Never attended school
+      TRUE ~ P27_ATTENDING_SCHOOL_P # Else, Attending school and in secondary (1)
+    )
+  ) |>
+  dplyr::filter(P5_AGE_P >= 14, P5_AGE_P <= 18) |>
+  dplyr::group_by(DIST_P, REGION_P) |>
+  dplyr::summarise(
+    total = n(),
+    attending_sec = sum(SecSchAtt == 1, na.rm = TRUE),
+    rate = round(attending_sec / total, 2)
+  ) |>
+  dplyr::mutate(
+    DIST_P_name = forcats::as_factor(DIST_P),
+    REGION_P_name = forcats::as_factor(REGION_P)
+  ) |>
+  dplyr::select(
+    DIST_P_name,
+    REGION_P_name,
+    everything()
+  ) |>
+  view()
+
+# wide table
+demographics |>
+  dplyr::mutate(
+    P28Sec = case_when(
+      P28_HIGHEST_LEVEL_P %in% 7:11 ~ 1,
+      TRUE ~ 2
+    ),
+    SecSchAtt = case_when(
+      P27_ATTENDING_SCHOOL_P == 1 & P28Sec == 2 ~ 3,
+      P26_EVER_ATTEND_SCHOOL_P == 2 ~ 4,
+      TRUE ~ P27_ATTENDING_SCHOOL_P
+    )
+  ) |>
+  dplyr::filter(P5_AGE_P >= 14, P5_AGE_P <= 18) |>
+  dplyr::group_by(DIST_P, REGION_P) |>
+  dplyr::summarise(
+    total = n(),
+    attending_sec = sum(SecSchAtt == 1, na.rm = TRUE),
+    rate = round(attending_sec / total, 2)
+  ) |>
+  dplyr::mutate(
+    DIST_P_name = forcats::as_factor(DIST_P),
+    REGION_P = forcats::as_factor(REGION_P)
+  ) |>
+  dplyr::select(
+    DIST_P_name,
+    REGION_P,
+    everything()
+  ) |>
+  tidyr::pivot_wider(
+    names_from =  REGION_P,
+    values_from = 4:6
+  ) |>
+  view()
+
+## III - EMPLOYMENT INDICATOR : Youth (20-24 years of age) not in employment, education or training (NEET)
+
+# Indicator Definition :
+# number of youth aged 20-24 not in employment, education or training *100 / total number of youth aged 20-24
+
+# Variables :
+# - P32_ACTIVITY_LAST_12_MONTHS_P = 
+# - P5_AGE_P = Age (20 - 24)
+
+# (a) Desegregate By District
 demographics |>
   dplyr::mutate(
     Employ = case_when(
@@ -112,15 +232,83 @@ demographics |>
       P32_ACTIVITY_LAST_12_MONTHS_P == 10 ~ 3
     )
   ) |>
-  dplyr::filter(P5_AGE_P >= 19, P5_AGE_P <= 24) |>
-  dplyr::group_by(DIST_P) %>%
+  dplyr::filter(P5_AGE_P >= 20, P5_AGE_P <= 24) |>
+  dplyr::group_by(DIST_P) |>
   summarise(
     total = n(),
     neet = sum(Employ == 2, na.rm = TRUE),
-    rate = neet / total
+    rate = round(neet / total, 2)
+  ) |>
+  dplyr::mutate(
+    DIST_P_name = forcats::as_factor(DIST_P),
+  ) |>
+  dplyr::select(
+    DIST_P_name,
+    everything()
   ) |>
   view()
 
+# (b) Desegregate by District & Rural/Urban
+
+# long table
+demographics |>
+  dplyr::mutate(
+    Employ = case_when(
+      P32_ACTIVITY_LAST_12_MONTHS_P %in% 1:6 ~ 1,
+      P32_ACTIVITY_LAST_12_MONTHS_P %in% 7:9 ~ 2,
+      P32_ACTIVITY_LAST_12_MONTHS_P == 11 ~ 2,
+      P32_ACTIVITY_LAST_12_MONTHS_P == 10 ~ 3
+    )
+  ) |>
+  dplyr::filter(P5_AGE_P >= 20, P5_AGE_P <= 24) |>
+  dplyr::group_by(DIST_P, REGION_P) |>
+  summarise(
+    total = n(),
+    neet = sum(Employ == 2, na.rm = TRUE),
+    rate = round(neet / total, 2)
+  ) |>
+  dplyr::mutate(
+    DIST_P_name = forcats::as_factor(DIST_P),
+    REGION_P_name = forcats::as_factor(REGION_P)
+  ) |>
+  dplyr::select(
+    DIST_P_name,
+    REGION_P_name,
+    everything()
+  ) |>
+  view()
+
+# wide table
+demographics |>
+  dplyr::mutate(
+    Employ = case_when(
+      P32_ACTIVITY_LAST_12_MONTHS_P %in% 1:6 ~ 1,
+      P32_ACTIVITY_LAST_12_MONTHS_P %in% 7:9 ~ 2,
+      P32_ACTIVITY_LAST_12_MONTHS_P == 11 ~ 2,
+      P32_ACTIVITY_LAST_12_MONTHS_P == 10 ~ 3
+    )
+  ) |>
+  dplyr::filter(P5_AGE_P >= 20, P5_AGE_P <= 24) |>
+  dplyr::group_by(DIST_P, REGION_P) |>
+  summarise(
+    total = n(),
+    neet = sum(Employ == 2, na.rm = TRUE),
+    rate = round(neet / total, 2)
+  ) |>
+  dplyr::mutate(
+    DIST_P_name = forcats::as_factor(DIST_P),
+    REGION_P = forcats::as_factor(REGION_P)
+  ) |>
+  dplyr::select(
+    DIST_P_name,
+    REGION_P,
+    everything()
+  ) |>
+  tidyr::pivot_wider(
+    names_from =  REGION_P,
+    values_from = 4:6
+  ) |>
+  view()
 
 # VISUALIZE ---------------------------------------------------------------
 
---
+++ input/SDG disaggregation_training_exercise/TESTSDG/input/symbology/GHS-DUTUC-L2.sld
@@ -0,0 +1,193 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<StyledLayerDescriptor xmlns="http://www.opengis.net/sld" xsi:schemaLocation="http://www.opengis.net/sld http://schemas.opengis.net/sld/1.1.0/StyledLayerDescriptor.xsd" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:ogc="http://www.opengis.net/ogc" version="1.1.0" xmlns:se="http://www.opengis.net/se">
+  <NamedLayer>
+    <se:Name>Classified_polygons</se:Name>
+    <UserStyle>
+      <se:Name>Classified_polygons</se:Name>
+      <se:FeatureTypeStyle>
+        <se:Rule>
+          <se:Name>11</se:Name>
+          <se:Description>
+            <se:Title>11</se:Title>
+          </se:Description>
+          <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
+            <ogc:PropertyIsEqualTo>
+              <ogc:PropertyName>DEGURBA_L2</ogc:PropertyName>
+              <ogc:Literal>11</ogc:Literal>
+            </ogc:PropertyIsEqualTo>
+          </ogc:Filter>
+          <se:PolygonSymbolizer>
+            <se:Fill>
+              <se:SvgParameter name="fill">#cdf57a</se:SvgParameter>
+            </se:Fill>
+            <se:Stroke>
+              <se:SvgParameter name="stroke">#232323</se:SvgParameter>
+              <se:SvgParameter name="stroke-width">1</se:SvgParameter>
+              <se:SvgParameter name="stroke-linejoin">bevel</se:SvgParameter>
+            </se:Stroke>
+          </se:PolygonSymbolizer>
+        </se:Rule>
+        <se:Rule>
+          <se:Name>12</se:Name>
+          <se:Description>
+            <se:Title>12</se:Title>
+          </se:Description>
+          <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
+            <ogc:PropertyIsEqualTo>
+              <ogc:PropertyName>DEGURBA_L2</ogc:PropertyName>
+              <ogc:Literal>12</ogc:Literal>
+            </ogc:PropertyIsEqualTo>
+          </ogc:Filter>
+          <se:PolygonSymbolizer>
+            <se:Fill>
+              <se:SvgParameter name="fill">#abcd66</se:SvgParameter>
+            </se:Fill>
+            <se:Stroke>
+              <se:SvgParameter name="stroke">#232323</se:SvgParameter>
+              <se:SvgParameter name="stroke-width">1</se:SvgParameter>
+              <se:SvgParameter name="stroke-linejoin">bevel</se:SvgParameter>
+            </se:Stroke>
+          </se:PolygonSymbolizer>
+        </se:Rule>
+        <se:Rule>
+          <se:Name>13</se:Name>
+          <se:Description>
+            <se:Title>13</se:Title>
+          </se:Description>
+          <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
+            <ogc:PropertyIsEqualTo>
+              <ogc:PropertyName>DEGURBA_L2</ogc:PropertyName>
+              <ogc:Literal>13</ogc:Literal>
+            </ogc:PropertyIsEqualTo>
+          </ogc:Filter>
+          <se:PolygonSymbolizer>
+            <se:Fill>
+              <se:SvgParameter name="fill">#375623</se:SvgParameter>
+            </se:Fill>
+            <se:Stroke>
+              <se:SvgParameter name="stroke">#232323</se:SvgParameter>
+              <se:SvgParameter name="stroke-width">1</se:SvgParameter>
+              <se:SvgParameter name="stroke-linejoin">bevel</se:SvgParameter>
+            </se:Stroke>
+          </se:PolygonSymbolizer>
+        </se:Rule>
+        <se:Rule>
+          <se:Name>21</se:Name>
+          <se:Description>
+            <se:Title>21</se:Title>
+          </se:Description>
+          <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
+            <ogc:PropertyIsEqualTo>
+              <ogc:PropertyName>DEGURBA_L2</ogc:PropertyName>
+              <ogc:Literal>21</ogc:Literal>
+            </ogc:PropertyIsEqualTo>
+          </ogc:Filter>
+          <se:PolygonSymbolizer>
+            <se:Fill>
+              <se:SvgParameter name="fill">#ffff00</se:SvgParameter>
+            </se:Fill>
+            <se:Stroke>
+              <se:SvgParameter name="stroke">#232323</se:SvgParameter>
+              <se:SvgParameter name="stroke-width">1</se:SvgParameter>
+              <se:SvgParameter name="stroke-linejoin">bevel</se:SvgParameter>
+            </se:Stroke>
+          </se:PolygonSymbolizer>
+        </se:Rule>
+        <se:Rule>
+          <se:Name>22</se:Name>
+          <se:Description>
+            <se:Title>22</se:Title>
+          </se:Description>
+          <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
+            <ogc:PropertyIsEqualTo>
+              <ogc:PropertyName>DEGURBA_L2</ogc:PropertyName>
+              <ogc:Literal>22</ogc:Literal>
+            </ogc:PropertyIsEqualTo>
+          </ogc:Filter>
+          <se:PolygonSymbolizer>
+            <se:Fill>
+              <se:SvgParameter name="fill">#a87000</se:SvgParameter>
+            </se:Fill>
+            <se:Stroke>
+              <se:SvgParameter name="stroke">#232323</se:SvgParameter>
+              <se:SvgParameter name="stroke-width">1</se:SvgParameter>
+              <se:SvgParameter name="stroke-linejoin">bevel</se:SvgParameter>
+            </se:Stroke>
+          </se:PolygonSymbolizer>
+        </se:Rule>
+        <se:Rule>
+          <se:Name>23</se:Name>
+          <se:Description>
+            <se:Title>23</se:Title>
+          </se:Description>
+          <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
+            <ogc:PropertyIsEqualTo>
+              <ogc:PropertyName>DEGURBA_L2</ogc:PropertyName>
+              <ogc:Literal>23</ogc:Literal>
+            </ogc:PropertyIsEqualTo>
+          </ogc:Filter>
+          <se:PolygonSymbolizer>
+            <se:Fill>
+              <se:SvgParameter name="fill">#732600</se:SvgParameter>
+            </se:Fill>
+            <se:Stroke>
+              <se:SvgParameter name="stroke">#232323</se:SvgParameter>
+              <se:SvgParameter name="stroke-width">1</se:SvgParameter>
+              <se:SvgParameter name="stroke-linejoin">bevel</se:SvgParameter>
+            </se:Stroke>
+          </se:PolygonSymbolizer>
+        </se:Rule>
+        <se:Rule>
+          <se:Name>30</se:Name>
+          <se:Description>
+            <se:Title>30</se:Title>
+          </se:Description>
+          <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
+            <ogc:PropertyIsEqualTo>
+              <ogc:PropertyName>DEGURBA_L2</ogc:PropertyName>
+              <ogc:Literal>30</ogc:Literal>
+            </ogc:PropertyIsEqualTo>
+          </ogc:Filter>
+          <se:PolygonSymbolizer>
+            <se:Fill>
+              <se:SvgParameter name="fill">#ff0000</se:SvgParameter>
+            </se:Fill>
+            <se:Stroke>
+              <se:SvgParameter name="stroke">#232323</se:SvgParameter>
+              <se:SvgParameter name="stroke-width">1</se:SvgParameter>
+              <se:SvgParameter name="stroke-linejoin">bevel</se:SvgParameter>
+            </se:Stroke>
+          </se:PolygonSymbolizer>
+        </se:Rule>
+        <se:Rule>
+          <se:Name></se:Name>
+          <se:Description>
+            <se:Title>DEGURBA_L2 is ''</se:Title>
+          </se:Description>
+          <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
+            <ogc:Or>
+              <ogc:PropertyIsEqualTo>
+                <ogc:PropertyName>DEGURBA_L2</ogc:PropertyName>
+                <ogc:Literal></ogc:Literal>
+              </ogc:PropertyIsEqualTo>
+              <ogc:PropertyIsNull>
+                <ogc:PropertyName>DEGURBA_L2</ogc:PropertyName>
+              </ogc:PropertyIsNull>
+            </ogc:Or>
+          </ogc:Filter>
+          <se:PolygonSymbolizer>
+            <se:Fill>
+              <se:SvgParameter name="fill">#4de798</se:SvgParameter>
+              <se:SvgParameter name="fill-opacity">0</se:SvgParameter>
+            </se:Fill>
+            <se:Stroke>
+              <se:SvgParameter name="stroke">#232323</se:SvgParameter>
+              <se:SvgParameter name="stroke-width">1</se:SvgParameter>
+              <se:SvgParameter name="stroke-linejoin">bevel</se:SvgParameter>
+            </se:Stroke>
+          </se:PolygonSymbolizer>
+        </se:Rule>
+      </se:FeatureTypeStyle>
+    </UserStyle>
+  </NamedLayer>
+</StyledLayerDescriptor>
